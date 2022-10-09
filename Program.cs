f17b544fd3b3b80dac8ba2536ee2c708b6880942
Micha J Dev <vyxisprime@gmail.com>
2022-10-10 01:14:01 +0200

    ZIp
    zip

--- Program.cs
+++ Program.cs
@@ -48,17 +48,18 @@ app.UseSwagger(options =>
 {
     options.SerializeAsV2 = true;
 });
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
-});
-app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = "swagger";
+});
+app.UseRouting();
+
+app.UseAuthorization();
 
 app.Run();
